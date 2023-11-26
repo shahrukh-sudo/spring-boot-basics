6468647815966e71731cec576ac0690d91482a59
shahrukh-sudo <57897724+shahrukh-sudo@users.noreply.github.com>
2023-11-26 08:43:50 +0530

    Initial commit

---
+++ src/main/java/com/shahrukh/Spring_Learnings/SpringLearningsApplication.java
@@ -0,0 +1,13 @@
+package com.shahrukh.Spring_Learnings;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringLearningsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringLearningsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shahrukh/Spring_Learnings/SpringLearningsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shahrukh.Spring_Learnings;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringLearningsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
